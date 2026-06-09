1f29edac6fad49075e0226200c44df9d57b21548
SChen <2496147886@qq.com>
2026-06-09 23:14:18 +0800

    Upload auto86 as cp1_6.9_auto86

--- cp1_6.9_auto86/vite.config.js
+++ cp1_6.9_auto86/vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@types/three': path.resolve(__dirname, 'node_modules/@types/three'),
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   server: {
     port: 5173,
-    open: true,
+    open: false
   },
+  build: {
+    target: 'esnext'
+  }
 });
