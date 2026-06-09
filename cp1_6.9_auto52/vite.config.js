1a90122ebe5173271ee86788510e4f81795ca853
SChen <2496147886@qq.com>
2026-06-09 23:14:10 +0800

    Upload auto52 as cp1_6.9_auto52

--- cp1_6.9_auto52/vite.config.js
+++ cp1_6.9_auto52/vite.config.js
@@ -1,12 +1,1 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  server: {
-    port: 5173,
-    open: false
-  },
-  build: {
-    outDir: 'dist',
-    target: 'es2020'
-  }
-});
+import { defineConfig } from
