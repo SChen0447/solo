8a9582ee66e2aefedb92c0dac08a6d2c6ad186c1
SChen <2496147886@qq.com>
2026-06-11 14:48:19 +0800

    Upload auto163 as cp1_6.11_auto163

---
+++ cp1_6.11_auto163/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: 'es2020',
+    sourcemap: 'hidden'
+  },
+  server: {
+    port: 5173,
+    host: true
+  }
+})
